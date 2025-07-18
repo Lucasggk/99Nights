634dd2a15b2138841ee6d32eb185af1f830e8614
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-17 23:49:24 -0300

    Create Bring.lua

---
+++ Bring.lua
@@ -0,0 +1,13 @@
+function bl(nome, parte)
+	local p = game.Players.LocalPlayer
+	local hrp = p.Character and p.Character:FindFirstChild("HumanoidRootPart")
+	if not hrp then return end
+	for _, item in ipairs(workspace.Items:GetChildren()) do
+		if item.Name == nome then
+			local obj = item:FindFirstChild(parte)
+			if obj and obj:IsA("BasePart") then
+				obj.CFrame = hrp.CFrame + Vector3.new(math.random(-5,5), 0, math.random(-5,5))
+			end
+		end
+	end
+end
