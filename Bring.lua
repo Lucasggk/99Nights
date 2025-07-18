d9a309929c6504ef62dd4a63ebd9666870421f53
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-18 00:04:35 -0300

    Update Bring.lua

--- Bring.lua
+++ Bring.lua
@@ -1,10 +1,16 @@
-function bl(nome, parte)
+function bl(nome, caminho)
 	local p = game.Players.LocalPlayer
 	local hrp = p.Character and p.Character:FindFirstChild("HumanoidRootPart")
 	if not hrp then return end
+
 	for _, item in ipairs(workspace.Items:GetChildren()) do
 		if item.Name == nome then
-			local obj = item:FindFirstChild(parte)
+			local partes = string.split(caminho, ".")
+			local obj = item
+			for _, parte in ipairs(partes) do
+				obj = obj:FindFirstChild(parte)
+				if not obj then break end
+			end
 			if obj and obj:IsA("BasePart") then
 				obj.CFrame = hrp.CFrame + Vector3.new(math.random(-5,5), 0, math.random(-5,5))
 			end
@@ -19,4 +25,20 @@ end
 
 function bringBolt()
 	bl("Bolt", "main")
+end
+
+function bringCoal()
+	bl("Coal", "Coal")
+end
+
+function bringCarrot()
+	bl("Carrot", "Handle")
+end
+
+function bringCoin()
+	bl("Coin Stack", "Mossy Coin.Main")
+end
+
+function bringLog()
+	bl("Log", "Main")
 end
