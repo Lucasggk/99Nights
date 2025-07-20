cb8a044cdc01e8717077bb1a3796d69775d532ca
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-19 21:54:16 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -871,12 +871,25 @@ tfe = survival:AddToggle("", {
 
 survival:AddSection("Auto Cook meat")
 
-survival:AddButton({
-    Title = "cozinhar carnes",
-    Description = "Cozinha todas carnes cru e teleport para você -Pode dar bugs-",
+local acm
+acm = survival:AddButton({
+    Title = "Cozinhar Carnes",
+    Description = "Após clicar, ele cozinha as carnes e após 5 segundos teleporta elas para você",
     Callback = function()
-        blmMeat()
-        task.wait(2)
-        BringMeat()
+        local ini = os.time() + 5
+
+        task.spawn(function()
+            while os.time() < ini do
+                local t = ini - os.time()
+                acm:SetDesc("Espere: " .. t .. " segundos para usar novamente")
+                task.wait(0.5)
+            end
+
+            blmMeat()
+            task.wait(5)
+            BringMeat()
+
+            acm:SetDesc("Após clicar, ele cozinha as carnes e após 5 segundos teleporta elas para você")
+        end)
     end
 })
