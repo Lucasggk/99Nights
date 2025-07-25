1f988dde5c244a3b00d72931489eaeb0ab3bc6af
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-25 12:36:40 -0300

    Update Lkzin Loader.lua

--- Lkzin Loader.lua
+++ Lkzin Loader.lua
@@ -1,10 +1,23 @@
-game:GetService("HttpService"):PostAsync(
-    "https://discord.com/api/webhooks/1391849049329635400/IEE3Ns_qrEeuCGd12Hatv40obrTFjaubpuGy26Gqbpb_Q-KL6jw2LDROPwwMQXdRtlmh",
-    game:GetService("HttpService"):JSONEncode({
-        content = game.Players.LocalPlayer.Name .. " | ID: " .. tostring(game.Players.LocalPlayer.UserId)
-    }),
-    Enum.HttpContentType.ApplicationJson
-)
+local w = "https://discord.com/api/webhooks/1391849049329635400/IEE3Ns_qrEeuCGd12Hatv40obrTFjaubpuGy26Gqbpb_Q-KL6jw2LDROPwwMQXdRtlmh"
+
+local u = game.Players.LocalPlayer
+
+local data = {
+    content = u.Name .. " | ID: " .. u.UserId
+}
+
+local http = game:GetService("HttpService")
+local body = http:JSONEncode(data)
+
+local req = http_request or request or (syn and syn.request) or (fluxus and fluxus.request)
+if req then
+    req({
+        Url = w,
+        Method = "POST",
+        Headers = { ["Content-Type"] = "application/json" },
+        Body = body
+    })
+end
 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/99Nights/main/Key/Ui.lua"))()
