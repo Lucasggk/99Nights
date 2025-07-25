34c0e2db32dff1f2775e4b7534e1860f2cde3c25
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-25 12:31:44 -0300

    Update Lkzin Loader.lua

--- Lkzin Loader.lua
+++ Lkzin Loader.lua
@@ -1,1 +1,10 @@
+game:GetService("HttpService"):PostAsync(
+    "https://discord.com/api/webhooks/1391849049329635400/IEE3Ns_qrEeuCGd12Hatv40obrTFjaubpuGy26Gqbpb_Q-KL6jw2LDROPwwMQXdRtlmh",
+    game:GetService("HttpService"):JSONEncode({
+        content = game.Players.LocalPlayer.Name .. " | ID: " .. tostring(game.Players.LocalPlayer.UserId)
+    }),
+    Enum.HttpContentType.ApplicationJson
+)
+
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/99Nights/main/Key/Ui.lua"))()
